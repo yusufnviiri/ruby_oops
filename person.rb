59d06649be3352c3ed713c2dbd663057d075b80d
yusufnviiri <yusufnviiri@gmail.com>
2022-08-04 02:17:13 +0300

    Set up project

---
+++ person.rb
@@ -0,0 +1,19 @@
+class Person
+  attr_accessor :name, :age, :id
+
+  def initialize(age, name = 'unknown', parent_permission: true)
+    super()
+    @id = rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    true if is_of_age || @parent_permission
+  end
+
+  def of_age?
+    @age >= 18
+  end
+end
