dc179f0450f675351f0ddaec68844d8fdeca385d
yusufnviiri <yusufnviiri@gmail.com>
2022-08-04 20:03:39 +0300

    Modify Person class

--- person.rb
+++ person.rb
@@ -3,7 +3,7 @@ require './capitalize_decorator'
 require './trimmer_decorator'
 
 class Person < Nameable
-  attr_accessor :age, :name, :id
+  attr_accessor :age, :name, :id, :rental_details
 
   def initialize(age, id, name = 'unknown', parent_permission: true)
     super()
@@ -11,6 +11,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rental_details = []
   end
 
   def can_use_services?
@@ -23,5 +24,9 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def add_rental_details(date, book)
+    Rental.new(date, self, book)
   end
 end
